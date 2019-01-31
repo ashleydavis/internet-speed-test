535ece0ce9294711f5ceb07f19781d3f57a128a0
Ashley Davis <ashley@codecapers.com.au>
2019-02-01 08:18:16 +1000

    Internet speed testing app.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # internet-speed-test
+
 Code for the Automated internet speed testing article on the The Data Wrangler
+
+## Pre req
+
+You need Node.js installed to run this.
+
+## Setup
+
+Clone or download and unpack this code repository.
+
+Then open a command line, change to the repo directory and install dependencies:
+
+    cd internet-speed-test
+    npm install
+
+Change to the pubic sub-directory and install bower dependencies:
+
+    cd public
+    bower install
+
+Change back to repo root directory:
+
+    cd ..
+
+## Run it
+
+Run the web server
+
+    npm start
+
+
+## View output
+
+Navigate your web browser to
+
+    http://localhost:8000/
+
---
+++ index.js
@@ -0,0 +1,194 @@
+'use strict';
+
+const fs = require('fs');
+const papa = require('papaparse');
+const api = require('fast-cli/api');
+const moment = require('moment');
+const cron = require('cron');
+const timeseriesAggregate = require('timeseries-aggregate');
+
+var outputFileName = "internet-speed-log.csv";
+
+var inProgress = false;
+
+var writeOutput = function (record) {
+    if (fs.existsSync(outputFileName)) {
+        fs.appendFileSync(outputFileName, "\r\n" + papa.unparse([record], { header: false }));
+    } else {
+        fs.writeFileSync(outputFileName, papa.unparse([record], { header: true }));
+    }
+};
+
+var testRun = function() {
+
+    var cronStart = new Date();
+    var cronStartStr = moment(cronStart).format('YYYY-MM-DD HH:mm:ss');
+    console.log("Executing cron job at: " + cronStartStr);
+
+    if (inProgress) {
+        console.log('Already in progress.')
+        return;
+    }
+
+    inProgress = true;
+
+    api((err, result) => {
+        if (err) {
+            console.error(err && err.stack || err);
+            inProgress = false;
+            return;
+        }
+
+        console.log(result);
+
+        if (result.isDone) {
+            inProgress = false;
+
+            var timeFinished = new Date();
+            var dateFinishedStr = moment(timeFinished).format('YYYY-MM-DD');
+            var timeFinishedStr = moment(timeFinished).format('HH:mm:ss');
+            var testTimeSeconds = moment(timeFinished).diff(cronStart, 'seconds');
+
+            var speed = result.speed;
+
+            if (result.unit === 'Kbps') {
+                speed = speed / 1000;
+            }
+
+            var data = {
+                Date: dateFinishedStr,
+                Time: timeFinishedStr,
+                SpeedMbps: speed,
+                TestTimeSeconds: testTimeSeconds, 
+            };
+
+            console.log(data);
+
+            writeOutput(data);
+        }
+    });
+};
+
+var CronJob = require('cron').CronJob;
+new CronJob('0 */10 * * * *', testRun, null, true, 'Australia/Brisbane');
+
+var express = require('express')
+var app = express()
+
+app.use(express.static('public'));
+
+var aggregateSeries = function (data, period) {
+
+    if (data.length === 0) {
+        return [];
+    }
+
+    var periodMs = periodToMs(period);
+    var periodStartDate = moment(data[0].Date).startOf(period).toDate(); 
+    var periodEndDate = moment(periodStartDate).add(periodMs, 'milliseconds');
+    var average = data[0].SpeedMbps;
+    var low = data[0].SpeedMbps;
+    var high = data[0].SpeedMbps;
+    var open = data[0].SpeedMbps;
+    var close = data[0].SpeedMbps;
+    var workingIndex = 1;
+
+    var averagedSeries = [];
+
+    while (workingIndex < data.length) {
+        var dataElement = data[workingIndex];
+        var curDate = dataElement.Date;
+
+        if (periodEndDate.isAfter(curDate)) {
+            // Add to current average.
+            average = (average + dataElement.SpeedMbps) / 2;
+            low = Math.min(low, dataElement.SpeedMbps);
+            high = Math.max(high, dataElement.SpeedMbps);
+            close = dataElement.SpeedMbps;
+        }
+        else {
+            averagedSeries.push({
+                Date: periodEndDate.toDate(),
+                SpeedMbps: average,
+                Open: open,
+                Close: close,
+                Low: low,
+                High: high,
+            });
+
+            // Start a new average.
+            periodStartDate = periodEndDate.toDate();
+            periodEndDate = moment(periodStartDate).add(periodMs, 'milliseconds');
+            average = dataElement.SpeedMbps;
+            low = dataElement.SpeedMbps;
+            high = dataElement.SpeedMbps;
+            open = dataElement.SpeedMbps;
+            close = dataElement.SpeedMbps
+        }
+
+        ++workingIndex;
+    }
+
+    averagedSeries.push({
+        Date: periodEndDate.toDate(),
+        SpeedMbps: average,
+        Open: open,
+        Close: close,
+        Low: low,
+        High: high,
+    });
+
+    return averagedSeries;
+};
+
+var periods = {
+    hour: 1 * 60 * 60 * 1000,
+    day: 1 * 24 * 60 * 60 * 1000,
+    week: 7 * 24 * 60 * 60 * 1000,
+};
+
+var periodToMs = function (period) {    
+    if (periods[period]) {
+        return periods[period];
+    }
+
+    throw new Error("Invalid period: " + period);
+};
+
+app.get('/internet-speed', (req, res) => {
+
+    var data = [];
+
+    try
+    {
+        if (fs.existsSync(outputFileName)) {
+            data = papa.parse(fs.readFileSync(outputFileName, 'utf8'), {
+                header: true,
+                dynamicTyping: true,
+            }).data;
+
+            data.forEach(record => {
+                // Merge data time.
+                record.Date = moment(record.Date + ' ' + record.Time, 'YYYY-MM-DD HH:mm:ss').toDate(),
+                console.log(record.Date);
+                delete record.Time;
+            });
+        }
+
+        if (req.query.period) {
+            data = aggregateSeries(data, req.query.period);
+        }
+ 
+        res.json(data);
+    }
+    catch (err) {
+        console.error(err && err.stack || err);
+        res.sendStatus(500);
+    }
+});
+
+app.listen(8000, function () {
+    console.log('Example app listening on port 8000!')
+});
+
+testRun();
